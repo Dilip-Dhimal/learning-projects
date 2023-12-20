1e12bb716921906fe8e55433474c9fd489db314d
Dilip-Dhimal <dilip.765511@pmc.tu.edu.np>
2023-12-20 11:15:08 +0545

    update

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+
+class AppColors{
+
+ static const backGroundColor =  Color(0xFFF2F5F9);
+ static const mainColor =  Colors.pink;
+ static const ratingColor = Color(0xFFFFC107);
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:podsriver/constants/colors.dart';
+import 'package:podsriver/provider/home_screen.dart';
+import 'package:podsriver/views/home_page.dart';
+
+
+
+
+void main () async{
+
+ runApp(ProviderScope(child: Home()));
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height =MediaQuery.of(context).size.height;
+    final width =MediaQuery.of(context).size.width;
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'Raleway',
+          colorSchemeSeed: AppColors.mainColor,
+          scaffoldBackgroundColor: AppColors.backGroundColor,
+        ),
+     home: HomeScreen(),
+
+    );
+  }
+}
+
